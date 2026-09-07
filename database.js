55e5ecd21fb107e84cc43c0f89d0f18774ddf4c3
juceilson <juceilsonjdc16@gmail.com>
2026-09-07 13:25:01 -0300

    configuração de persistencia

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: ":memory:", // db.sqlite - para persistencia en disco
+  storage: "db.sqlite", // :memory: - para salvamento em memória
   logging: false,
 });
 
